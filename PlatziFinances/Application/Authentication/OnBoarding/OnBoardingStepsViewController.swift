2efc4f345f223925dad8a26efeeba6933983ea92
Jhonyfer Angarita Moreno <40864812+Viejony@users.noreply.github.com>
2022-06-20 10:28:39 -0500

    Added transactions views

---
+++ PlatziFinances/Application/Authentication/OnBoarding/OnBoardingStepsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  OnBoardingStepsViewController.swift
+//  PlatziFinances
+//
+//  Created by Jhonyfer Angarita on 19/06/22.
+//
+
+import UIKit
+
+class OnBoardingStepsViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var onBoardingImage: UIImageView!
+    
+    var item: OnBoardingItem?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = item?.title
+        descriptionLabel.text = item?.title
+        onBoardingImage.image = UIImage(named: item?.imageName ?? "")
+        
+    }
+}
--- PlatziFinances/Application/Authentication/OnBoarding/OnBoardingViewController.swift
+++ PlatziFinances/Application/Authentication/OnBoarding/OnBoardingViewController.swift
@@ -7,25 +7,101 @@
 
 import UIKit
 
+struct OnBoardingItem{
+    let title: String
+    let description: String
+    let imageName: String
+}
+
 class OnBoardingViewController: UIPageViewController {
     
-    @IBOutlet weak var pageControl: UIPageControl!
+    var pageControl: UIPageControl?
+    
+    fileprivate(set) lazy var items: [OnBoardingItem] = {
+        return [
+            OnBoardingItem(
+                title: "Save money and reduce debt",
+                description: "Press the start button",
+                imageName: "OnBoarding1"),
+            OnBoardingItem(
+                title: "You,ve finished your onboarding",
+                description: "Press the start button",
+                imageName: "OnBoarding2"),
+        ]
+    }()
+    
+    fileprivate(set) lazy var contentViewController: [UIViewController] = {
+        var items = [UIViewController]()
+        for i in 0..<self.items.count{
+            items.append(self.instanceViewController(i))
+        }
+        return items
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        delegate = self
+        dataSource = self
+        
+        pageControl?.numberOfPages = items.count
+        updateContainerView(stepNumber: 0)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateContainerView(stepNumber index: Int){
+        setViewControllers(
+            [contentViewController[index]],
+            direction: .forward,
+            animated: true)
     }
-    */
+    
+    func instanceViewController(_ index: Int) -> UIViewController{
+        guard let viewController = UIStoryboard(name: "OnBoarding", bundle: Bundle.main).instantiateViewController(withIdentifier: "OnBoardingSteps") as? OnBoardingStepsViewController else {
+            return UIViewController()
+        }
+        
+        viewController.item = items[index]
+        return viewController
+    }
 
+}
+
+extension OnBoardingViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let index = contentViewController.firstIndex(of: viewController)
+        if index == 0 {
+            return nil
+        }
+        return contentViewController[index! + 1]
+    }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            let index = contentViewController.firstIndex(of: viewController)
+        if index == contentViewController.count - 1 {
+            return nil
+        }
+          return contentViewController[index! + 1]
+    }
+}
+
+extension OnBoardingViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        didFinishAnimating finished: Bool,
+        previousViewControllers: [UIViewController],
+        transitionCompleted completed: Bool)
+    {
+        guard let index = contentViewController.firstIndex(of: viewControllers!.first!) else{
+            return
+        }
+        
+        pageControl?.currentPage = index
+    }
 }
